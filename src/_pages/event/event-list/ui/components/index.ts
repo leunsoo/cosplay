12b1c81c84a51b7080649a992af85c444988fdbf
leunsoo <silverwaterl@naver.com>
2026-08-10 23:23:55 +0900

    chore: remove dead code

--- src/_pages/event/event-list/ui/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { HeroBanner } from './HeroBanner';
-export { EventSourceFilter } from './EventSourceFilter';
-export { EventFilter } from './EventFilter';
-export { EventCalendarView } from './EventCalendarView';
-export { LoadMoreButton } from './LoadMoreButton';
