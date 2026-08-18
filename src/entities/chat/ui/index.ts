d774b9a43589599494a7454730623c7786a2b896
leunsoo <silverwaterl@naver.com>
2026-08-18 21:26:00 +0900

    chore: 파일명 변경 및 barrel export 정리

--- src/entities/chat/ui/index.ts
+++
@@ -1,4 +0,0 @@
-export { GroupChatHeader } from './GroupChatHeader';
-export { GroupMessageInput } from './GroupMessageInput';
-export { ChatMessageBubble } from './ChatMessageBubble';
-export type { ChatMessage } from '../model';
